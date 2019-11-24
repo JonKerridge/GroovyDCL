f3d39229c4e467c9979493faf0f4edc3a9edb9cd
JonKe <jonkerridge@btinternet.com>
2019-11-24 19:23:28 +0000

    Added main program versions to overcome Intellij groovy start problem

---
+++ src/main/groovy/groovyDCL/MainPO2.groovy
@@ -0,0 +1,24 @@
+package groovyDCL
+
+import groovyJCSP.PAR
+import jcsp.net2.NetChannel
+import jcsp.net2.Node
+import jcsp.net2.mobile.CodeLoadingChannelFilter
+import jcsp.net2.tcpip.TCPIPNodeAddress
+
+class MainPO2 {
+  public static void main(String[] args) {
+    def sendIP = "127.0.0.1"
+    def po1IP = "127.0.0.2"
+    def po2IP = "127.0.0.3"
+
+    def po2IPAddress = new TCPIPNodeAddress(po2IP, 1000)
+    Node.getInstance().init(po2IPAddress)
+
+    def fromPrevious = NetChannel.numberedNet2One( 1, new CodeLoadingChannelFilter.FilterRX())
+
+    def po2 = new PassOn2(fromPrevious: fromPrevious)
+
+    new PAR([po2]).run()
+  }
+}
---
+++ src/main/groovy/groovyDCL/MainSend.groovy
@@ -0,0 +1,25 @@
+package groovyDCL
+
+import groovyJCSP.PAR
+import jcsp.net2.NetChannel
+import jcsp.net2.Node
+import jcsp.net2.mobile.CodeLoadingChannelFilter
+import jcsp.net2.tcpip.TCPIPNodeAddress
+
+class MainSend {
+  public static void main(String[] args) {
+    def sendIP = "127.0.0.1"
+    def po1IP = "127.0.0.2"
+    def po2IP = "127.0.0.3"
+
+    def sendAddress = new TCPIPNodeAddress(sendIP, 1000)
+    Node.getInstance().init(sendAddress)
+
+    def po1IPAddress = new TCPIPNodeAddress(po1IP, 1000)
+    def toNext = NetChannel.one2net(po1IPAddress, 1, new CodeLoadingChannelFilter.FilterTX())
+
+    def send = new Sender(toNext: toNext)
+
+    new PAR([send]).run()
+  }
+}
